81ca2d23af016c781bd9b2113757e272207a6295
Md. Asaduzzaman Shuvo <100297947+ashuvo25@users.noreply.github.com>
2023-11-09 00:15:03 +0600

    Update Fibonacchi.cpp

--- Algorithm/Number Theory/GCD/Fibonacchi.cpp
+++ Algorithm/Number Theory/GCD/Fibonacchi.cpp
@@ -3,21 +3,14 @@
 using namespace std;
 
 int fibo(int a,int b,int n){
-
     if(n == 0|| n==1) return a;
-
     return fibo(b,a+b,n-1);
-
 }
-
-int main()
-{
+int main(){
 int a=0;int b=1;
 int n;
 cin >>n ;
 int v=fibo(a,b,n);
 cout<< v <<endl;
-
-///main();
 return 0;
 }
